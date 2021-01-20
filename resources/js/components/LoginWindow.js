76941d3abbd83816c285c9bed4ffdca8212a9e93
alekwitcode <Alekwit.code@gmail.com>
2021-01-20 04:41:24 +0100

    Initial setup

---
+++ resources/js/components/LoginWindow.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../../css/app.css';
+
+function LoginWindow() {
+    return (
+        <div className="container-fluid">
+            <div className="window">
+                <form>
+                    <h2>Twitter Login</h2>
+                    <label>
+                        Login:
+                        <input type="text" name="login" />
+                    </label>
+                    <label>
+                        Password:
+                        <input type="password" name="password" />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default LoginWindow;
+
+if (document.getElementById('login')) {
+    ReactDOM.render(<LoginWindow />, document.getElementById('login'));
+}
